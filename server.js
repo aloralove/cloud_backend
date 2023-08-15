dd61b630970c80ee2d721b8be9ee0318918221ef
Alora Greer <agreer5@alaska.edu>
2023-08-15 14:44:27 -0800

    connect to bigquery

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const {BigQuery} = require('@google-cloud/bigquery');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const bigquery = new BigQuery();
+
+app.get('/data', async (req, res) => {
+  const query = `SELECT * FROM \`sodium-airport-395522.alora_purpleair_dataset.processed\` LIMIT 10`; // Update this query as needed
+  const [rows] = await bigquery.query(query);
+  
+  res.send(rows);
 });
 
 app.listen(PORT, () => {
