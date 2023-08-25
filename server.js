1aabc8e67d49f97316cbca32be6233f395d89bdc
Alora Greer <agreer5@alaska.edu>
2023-08-25 14:49:15 -0800

    prot 8080

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {BigQuery} = require('@google-cloud/bigquery');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const cors = require('cors');
 
 const bigquery = new BigQuery();
