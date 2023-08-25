743e11e1aff504181d9da4a4e2b76ac26030fcf1
Alora Greer <agreer5@alaska.edu>
2023-08-25 14:40:40 -0800

    add cors

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const {BigQuery} = require('@google-cloud/bigquery');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const bigquery = new BigQuery();
-
+app.use(cors());
 app.get('/data', async (req, res) => {
   const query = `SELECT * FROM \`sodium-airport-395522.alora_purpleair_dataset.processed\` LIMIT 10`; // Update this query as needed
   const [rows] = await bigquery.query(query);
